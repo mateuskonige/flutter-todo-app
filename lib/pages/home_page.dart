904cd5dbb58a99c146e28a13ec0caa68ca20ccac
Mateus Reis <mateus.reis@com3telecom.com.br>
2026-01-06 09:40:13 -0300

    refactor: ListView -> ListView.builder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,31 +9,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void checkTodo() {}
+  List toDoList = [
+    ["Make work", false],
+    ["Make exercise", true],
+    ["Make food", false],
+  ];
+
+  void changeCheck(bool? value, int index) {
+    toDoList[index][1] = !toDoList[index][1];
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber[100],
       appBar: AppBar(title: Text("To Do App")),
-      body: ListView(
-        children: [
-          TodoTile(
-            taskName: "Make work",
-            isCompleted: false,
-            onChanged: (e) {},
-          ),
-          TodoTile(
-            taskName: "Make exercise",
-            isCompleted: true,
-            onChanged: (e) {},
-          ),
-          TodoTile(
-            taskName: "Make food",
-            isCompleted: false,
-            onChanged: (e) {},
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: "${index + 1}. ${toDoList[index][0]}",
+            isCompleted: toDoList[index][1],
+            onChanged: (value) => changeCheck(value, index),
+          );
+        },
       ),
     );
   }
